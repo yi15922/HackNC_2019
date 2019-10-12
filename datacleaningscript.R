f4791b1d42d04c4c28ba84ff860eb06bd5ce1176
Margaret Reed <margaret.reed@duke.edu>
2019-10-12 12:33:09 -0400

    cleaning more

---
+++ datacleaningscript.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(broom)
+library(dplyr)
+library(lubridate)
+
+data <- read_csv("data.csv") %>%
+  tidy()
+
+data <- data %>%
+  mutate(dateTime = ymd_hms(CreatedDate),
+         Year = year(dateTime),
+         Month = month(dateTime),
+         Day = day(dateTime),
+         Date = paste(Year, Month, Day, sep = "-") %>% ymd() %>% as.Date() %>%
+           select(data$Date, everything())
+
